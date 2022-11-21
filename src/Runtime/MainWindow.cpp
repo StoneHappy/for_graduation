1a351b8d20436dd7a79f5a2afbf81dd715a7d3db
StoneHappy <18017158729@163.com>
2022-11-22 00:09:19 +0800

    ✨ feat: Debuglog

--- src/Runtime/MainWindow.cpp
+++ src/Runtime/MainWindow.cpp
@@ -2,12 +2,25 @@
 #include "ui_MainWindow.h"
 #include <QMdiArea>
 #include <QLoggingCategory>
+#include <QPlainTextEdit>
+#include <QPointer>
+static QPointer<QPlainTextEdit> s_messageLogWidget;
+static void messageHandler(QtMsgType msgType, const QMessageLogContext& logContext, const QString& text)
+{
+	if (!s_messageLogWidget.isNull())
+		s_messageLogWidget->appendPlainText(text);
+}
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow),
     m_vulkanWindow(new CS::VulkanWindow())
 {
     ui->setupUi(this);
+
+	// Debug log
+	s_messageLogWidget = ui->debugLog;
+	s_messageLogWidget->setReadOnly(true);
+	qInstallMessageHandler(messageHandler);
     QWidget* vulkanWindowWrapper = QWidget::createWindowContainer(m_vulkanWindow);
 	QLoggingCategory::setFilterRules(QStringLiteral("qt.vulkan=true"));
 
