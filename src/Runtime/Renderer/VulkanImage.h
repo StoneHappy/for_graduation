215b1dae619358c152a1f2ddf28fbccacc237ef2
StoneHappy <18017158729@163.com>
2022-11-24 19:20:38 +0800

    ✨ feat: createtextureimage

--- src/Runtime/Renderer/VulkanImage.h
+++ src/Runtime/Renderer/VulkanImage.h
@@ -8,6 +8,7 @@
 namespace GU
 {
     void createTextureImage(const VulkanContext& vulkanContext, const char* picturePath, VulkanImage& image);
-
     void createImage(const VulkanContext& vulkanContext, uint32_t width, uint32_t height, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage, VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
+    void transitionImageLayout(const VulkanContext& vulkanContext, VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout);
+    void copyBufferToImage(const VulkanContext& vulkanContext, VkBuffer buffer, VkImage image, uint32_t width, uint32_t height);
 }
