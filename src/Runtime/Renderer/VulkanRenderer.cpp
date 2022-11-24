e95cf9a4b8c79a170e8adde760918296dd12ed8b
StoneHappy <18017158729@163.com>
2022-11-24 11:04:29 +0800

    ✨ feat: ubo shader

--- src/Runtime/Renderer/VulkanRenderer.cpp
+++ src/Runtime/Renderer/VulkanRenderer.cpp
@@ -1,10 +1,9 @@
 #include "VulkanRenderer.h"
 #include <QVulkanDeviceFunctions>
 #include <Renderer/VulkanShader.h>
-#include "j_shader_modules_vert.h"
-#include "j_shader_modules_frag.h"
-#include "r_descriptor_layout_buffer_vert.h"
-#include "r_descriptor_layout_buffer_frag.h"
+#include "shader_ubo_vert.h"
+#include "shader_ubo_frag.h"
+
 #include <Renderer/VulkanGraphicsPipeline.h>
 #include <Renderer/VulkanBuffer.h>
 namespace GU
@@ -25,8 +24,8 @@ namespace GU
 		m_vulkanContext.graphicsQueue = m_window->graphicsQueue();
 		m_vulkanContext.renderPass = m_window->defaultRenderPass();
 
-		VkShaderModule vertexShader = createShader(m_window->device(), r_descriptor_layout_buffer_vert, sizeof(r_descriptor_layout_buffer_vert));
-		VkShaderModule fragShader = createShader(m_window->device(), r_descriptor_layout_buffer_frag, sizeof(r_descriptor_layout_buffer_frag));
+		VkShaderModule vertexShader = createShader(m_window->device(), shader_ubo_vert, sizeof(shader_ubo_vert));
+		VkShaderModule fragShader = createShader(m_window->device(), shader_ubo_frag, sizeof(shader_ubo_frag));
 		createShaderStageInfo(vertexShader, fragShader, m_vulkanContext.shaderStage);
 		createDescriptorSetLayout(m_vulkanContext, m_vulkanContext.descriptorSetLayout);
 		createPipelineLayout(m_vulkanContext, m_vulkanContext.descriptorSetLayout, m_vulkanContext.pipelineLayout);
@@ -105,6 +104,7 @@ namespace GU
 			VkDeviceSize offsets[] = { 0 };
 			m_devFuncs->vkCmdBindVertexBuffers(cmdBuf, 0, 1, vertexBuffers, offsets);
 			m_devFuncs->vkCmdBindIndexBuffer(cmdBuf, m_vulkanContext.indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+			m_devFuncs->vkCmdBindDescriptorSets(cmdBuf, VK_PIPELINE_BIND_POINT_GRAPHICS, m_vulkanContext.pipelineLayout, 0, 1, &m_vulkanContext.descriptorSets[m_window->currentSwapChainImageIndex()], 0, nullptr);
 			//m_devFuncs->vkCmdDraw(cmdBuf, 3, 1, 0, 0);
 			m_devFuncs->vkCmdDrawIndexed(cmdBuf, 6, 1, 0, 0, 0);
 		m_devFuncs->vkCmdEndRenderPass(cmdBuf);
