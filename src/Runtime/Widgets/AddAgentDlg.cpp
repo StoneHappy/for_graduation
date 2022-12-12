5e74bf2b85ccb663e26e8e4d13fea365a37e4d6d
StoneHappy <18017158729@163.com>
2022-12-13 00:43:45 +0800

    ✨ feat: can rotate right

--- src/Runtime/Widgets/AddAgentDlg.cpp
+++ src/Runtime/Widgets/AddAgentDlg.cpp
@@ -11,6 +11,13 @@
 #include <Scene/Entity.h>
 #include <Function/Animation/Animation.h>
 #include <Function/AgentNav/RCScheduler.h>
+
+#define GLM_ENABLE_EXPERIMENTAL
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/gtx/quaternion.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 AddAgentDlg::AddAgentDlg(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AddAgentDlg)
@@ -34,6 +41,13 @@ void AddAgentDlg::on_pushButtonOK_clicked()
 	materialComponent.currentAnimation = GLOBAL_ANIMATION->getAnimationsWithUUID(GLOBAL_ASSET->getSkeletalMeshWithUUID(modelitem->data().toULongLong())->meshs[0].animationID).begin()->first;
 	auto&& transform = entity.getComponent<GU::TransformComponent>();
 	transform.Translation = GLOBAL_RCSCHEDULER->hitPos;
+	glm::vec3 org = glm::normalize(glm::vec3(0, 0, 1.0));
+	glm::vec3 dest = glm::normalize(glm::vec3(1.0, 0.0, -1.0));
+	auto rot = glm::rotation(org, dest);
+	auto euler = glm::eulerAngles(rot);
+	transform.Rotation.x = euler.x;
+	transform.Rotation.y = euler.y;
+	transform.Rotation.z = euler.z;
 }
 AddAgentDlg::~AddAgentDlg()
 {
